5a2441b95a789f160a7a28f25a3a59dc678519fb
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2021-10-11 12:54:29 +0300

    DES_EDE implemented

---
+++ des/des_ede.py
@@ -0,0 +1,24 @@
+from des import DES, Data
+
+
+class DES_EDE:
+
+    @staticmethod
+    def encrypt(data: str, key: str) -> Data:
+        enc1 = DES.encrypt(data, key)
+        dec1 = DES.decrypt(enc1, key)
+        enc2 = DES.encrypt(dec1.to_raw(), key)
+        return enc2
+
+    @staticmethod
+    def decrypt(data: Data, key: str) -> Data:
+        dec1 = DES.decrypt(data, key)
+        enc1 = DES.encrypt(dec1.to_raw(), key)
+        dec2 = DES.decrypt(enc1, key)
+        return dec2
+
+if __name__ == '__main__':
+    key = '123'
+    a = DES3_EDE.encrypt('test test test', key)
+    b = DES3_EDE.decrypt(a, key)
+    print(b.to_raw())
