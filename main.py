f347367c78c3bb2f1e493cdbc2318ee9780414fd
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2021-12-05 23:48:35 +0300

    update main

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from rsa.rsa import RSA, Data
+from blake.blake import Blake
 import time
 
 
-if __name__=='__main__':
+def rsa_test():
     start = time.perf_counter()
     keys = RSA.generate_keys()
     end = time.perf_counter()
     print(f"Key generation time: \t{end - start}")
 
     raw = 'Test test test'
-    
+    print('Encrypting:', raw)
+
     start = time.perf_counter()
     encrypted = RSA.encrypt(raw, keys.public)
     end = time.perf_counter()
     print(f"Encryption time: \t{end - start}")
+
+    print('Encrypted:', encrypted.to_hex())
 
     start = time.perf_counter()
     decrypted = RSA.decrypt(encrypted, keys.private)
@@ -22,3 +26,16 @@ if __name__=='__main__':
 
     print(f"Decrypted: {decrypted.to_raw()}")
 
+
+def blake_test():
+    blake = Blake()
+    data = b''
+    with open('/etc/passwd', 'rb') as f:
+        data = f.read()
+    digest = blake.hexdigest(data)
+    print(digest)
+
+
+if __name__ == '__main__':
+    # rsa_test()
+    blake_test()
